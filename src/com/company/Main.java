a6a95bcbaa9ed7e023ad1237ba3ab5abb2660a6f
main <mokomokohituji@users.noreply.github.com>
2021-06-27 23:49:17 +0900

    最大値を求めるプログラム

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// アルゴリズムの教本を読みながら実践中
+        Scanner scan = new Scanner(System.in);
+        System.out.println("4つの整数の最大値を求めます");
+        System.out.println("aの値"); int a = scan.nextInt();
+        System.out.println("bの値"); int b = scan.nextInt();
+        System.out.println("cの値"); int c = scan.nextInt();
+        System.out.println("dの値"); int d = scan.nextInt();
+
+        int max = a;
+        if ( b > max) max = b;
+        if ( c > max) max = c;
+        if ( d > max) max = d;
+        System.out.println("最大値は" + max + "です。");
     }
 }
