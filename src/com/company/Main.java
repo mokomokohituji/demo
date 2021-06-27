359565e2559befc8639a372338b9969d045904aa
main <mokomokohituji@users.noreply.github.com>
2021-06-28 00:31:50 +0900

    最大値を求める比較部分を別のメソッドに分割した

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// アルゴリズムの教本を読みながら実践中
+
+        //コンソールにキーボードで入力した数値を受け取る
         Scanner scan = new Scanner(System.in);
         System.out.println("4つの整数の最大値を求めます");
         System.out.println("aの値"); int a = scan.nextInt();
@@ -13,10 +15,15 @@ public class Main {
         System.out.println("cの値"); int c = scan.nextInt();
         System.out.println("dの値"); int d = scan.nextInt();
 
+        scan(a,b,c,d);
+    }
+    //もとのソースから分割をしてみる
+    public static void scan(int a,int b, int c,int d){
         int max = a;
         if ( b > max) max = b;
         if ( c > max) max = c;
         if ( d > max) max = d;
         System.out.println("最大値は" + max + "です。");
     }
+
 }
